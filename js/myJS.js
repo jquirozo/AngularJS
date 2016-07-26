1d68af785cb2cec3fd07138e18f4729694358777
Jacobo Quiroz <Jacobo Quiroz>
2016-07-26 11:57:06 -0500

    04-Entender scope y agregar bootstrap
    A diferencia del tutorial, decidí usar bootstrap para usar sus estilos y
    características responsivas

--- js/myJS.js
+++ js/myJS.js
@@ -1,9 +1,10 @@
 var myApp = angular.module('myApp',[]);//creating app
-		myApp.controller('AlumnosController', ['$scope', function($scope) { // creating controller
-		  $scope.alumnos =[
+	myApp.controller('AlumnosController', ['$scope', function($scope) { // creating controller
+	    $scope.alumnos =[
 				{nombre: "Jacobo Quiroz", curso: "3ro ESO", telefono:"1234567890"},
 				{nombre: "Rosa Perez", curso: "1ro ESO", telefono:"1234567891"},
 				{nombre: "Alberto Herrera", curso: "2do ESO", telefono:"1234567892"},
 				{nombre: "Ana Mariño", curso: "3ro ESO", telefono:"1234567893"}	
-			];
-		}]);		
+		];
+		
+	}]);				

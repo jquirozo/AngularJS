34edc3a2b4025a7e4e2646e81e2eae753df3ca02
Jacobo Quiroz <Jacobo Quiroz>
2016-07-26 13:06:32 -0500

    06 Agregando registros
    Se crea un formulario básico para agregar registros a la lista de
    alumnos.

--- js/myJS.js
+++ js/myJS.js
@@ -1,10 +1,18 @@
 var myApp = angular.module('myApp',[]);//creating app
-	myApp.controller('AlumnosController', ['$scope', function($scope) { // creating controller
-	    $scope.alumnos =[
+myApp.controller('AlumnosController', ['$scope', function($scope) { // creating controller
+    $scope.alumnos =[
 				{nombre: "Jacobo Quiroz", curso: "3ro ESO", telefono:"1234567890"},
 				{nombre: "Rosa Perez", curso: "1ro ESO", telefono:"1234567891"},
 				{nombre: "Alberto Herrera", curso: "2do ESO", telefono:"1234567892"},
 				{nombre: "Ana Mariño", curso: "3ro ESO", telefono:"1234567893"}	
-		];
-		
-	}]);				
+	];
+
+	$scope.Save = function(){
+		$scope.alumnos.push({
+			nombre:$scope.nuevoAlumno.nombre,
+			telefono:$scope.nuevoAlumno.telefono,
+			curso:$scope.nuevoAlumno.curso
+		});
+	}
+
+}]);
